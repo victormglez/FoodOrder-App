a68d1d6283341bdd1ac8d29ca28dc4ca29819c7e
victormglez <victormgb99@gmail.com>
2024-08-01 17:16:45 -0600

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import logoImg from '../assets/logo.jpg';
+import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
+
+export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumber, item) => {
+    return totalNumber + item.quantity;
+  }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="Logo APP" />
+        <h1>Food Order</h1>
+      </div>
+      <nav>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
+      </nav>
+    </header>
+  );
+}
